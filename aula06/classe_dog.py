b5fa539e890eeddba09a5ac1a999f89130bdb8a0
Peter <Peterfernandestorres@gmail.com>
2023-12-05 11:16:25 -0300

    Adeus Senac

---
+++ aula06/classe_dog.py
@@ -0,0 +1,16 @@
+import os
+os.system ("clear")
+
+class Dog:
+    def __init__ (self, name , age ):
+        self.name = name
+        self.age = age
+def data_dog (self):
+    print (self.name)
+    print (self.age)
+
+def sit (self):
+    print ("Sentou")
+
+def sit (self):
+    print ("Rolou")
---
+++ aula06/obj_dog.py
@@ -0,0 +1,9 @@
+import classe_dog
+
+pastor = classe_dog.Dog ("bob",2)
+pastor.data_dog ()
+pastor.sit ()
+pastor.roll_over ()
+
+print (pastor.__class__)
+print (pastor.__dict__)
